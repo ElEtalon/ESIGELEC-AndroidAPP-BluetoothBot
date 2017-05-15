a207d517d0acafad752bdd88eea94312aa2af9fc
paul bouquet <paul.bouquet@gmail.com>
2017-05-15 18:15:13 +0200

    Logo + First XML templates

--- app/src/main/java/fr/esigelec/bluetoothbot/HomeActivity.java
+++ app/src/main/java/fr/esigelec/bluetoothbot/HomeActivity.java
@@ -2,6 +2,8 @@ package fr.esigelec.bluetoothbot;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    public void doGoAbout(View v)
+    {
+        Toast.makeText(this, "Button Clicked", Toast.LENGTH_LONG).show();
     }
 }
