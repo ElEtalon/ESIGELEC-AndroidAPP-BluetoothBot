e06b8e0d2f583fd755d30ed46a71365c363f9821
paul bouquet <paul.bouquet@gmail.com>
2017-05-15 19:14:04 +0200

    [HomeActivity] ListView OK --> go to controls on click

--- app/src/main/java/fr/esigelec/bluetoothbot/HomeActivity.java
+++ app/src/main/java/fr/esigelec/bluetoothbot/HomeActivity.java
@@ -3,18 +3,48 @@ package fr.esigelec.bluetoothbot;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
+import android.widget.ListView;
 import android.widget.Toast;
 import android.content.Intent;
+
+import java.util.ArrayList;
 
 public class HomeActivity extends AppCompatActivity {
 
     ImageButton imgAboutButton;
+    ListView listViewDevices;
+
+    String[] devices = new String[] { "Device0", "Device3", "Device2", "Device1"};
+    final ArrayList<String> deviceList = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        /// LIST VIEW
+        // Devices
+        listViewDevices =(ListView)findViewById(R.id.listViewDevices);
+
+        // List view devices update
+        for (int i = 0; i < devices.length; ++i) {
+            deviceList.add(devices[i]);
+        }
+        listViewDevices.setAdapter(new ArrayAdapter(this, android.R.layout.simple_list_item_1, deviceList));
+
+
+        // ON CLICK
+        listViewDevices.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(), "ListView item : " + position + " & id : " + deviceList.get((int)id), Toast.LENGTH_LONG).show();
+                Intent controlsPage=new Intent(HomeActivity.this, ControlsActivity.class);
+                startActivity(controlsPage);
+            }
+        });
 
         imgAboutButton =(ImageButton)findViewById(R.id.imageAboutButton);
         imgAboutButton.setOnClickListener(new View.OnClickListener() {
@@ -27,11 +57,4 @@ public class HomeActivity extends AppCompatActivity {
             }
         });
     }
-
-    /*
-    public void onClick(View v){
-        if(v.getId()==R.id.imageAboutButton) {
-            Toast.makeText(getApplicationContext(), "About button clicked", Toast.LENGTH_LONG).show();
-        }
-    }*/
 }
