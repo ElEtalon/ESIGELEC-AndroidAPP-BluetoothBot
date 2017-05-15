77e25ef782a6d4c616ad385457946981ef7b80bd
paul bouquet <paul.bouquet@gmail.com>
2017-05-15 18:34:09 +0200

    [HomeActivity] Relative Layout + Notification when click

--- app/src/main/java/fr/esigelec/bluetoothbot/HomeActivity.java
+++ app/src/main/java/fr/esigelec/bluetoothbot/HomeActivity.java
@@ -3,18 +3,24 @@ package fr.esigelec.bluetoothbot;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 public class HomeActivity extends AppCompatActivity {
+
+    ImageButton imgAboutButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-    }
 
-    public void doGoAbout(View v)
-    {
-        Toast.makeText(this, "Button Clicked", Toast.LENGTH_LONG).show();
+        imgAboutButton =(ImageButton)findViewById(R.id.imageAboutButton);
+        imgAboutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "About button clicked", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
