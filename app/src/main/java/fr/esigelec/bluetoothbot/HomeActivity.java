d7f63d84939abb441761668c32d3de82011c34c4
paul bouquet <paul.bouquet@gmail.com>
2017-05-15 22:53:06 +0200

    [HomeActivity] Add switch button (display discover or paired devices)

--- app/src/main/java/fr/esigelec/bluetoothbot/HomeActivity.java
+++ app/src/main/java/fr/esigelec/bluetoothbot/HomeActivity.java
@@ -9,50 +9,83 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.RadioGroup;
+import android.widget.Switch;
 import android.widget.Toast;
 import android.content.Intent;
 
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Set;
 
 public class HomeActivity extends AppCompatActivity {
 
-    ImageButton imgAboutButton;
-    ListView listViewDevices;
-    final ArrayList<String> deviceList = new ArrayList<String>();
+    private ImageButton imgAboutButton;
+    private Switch switchButton;
+    private ListView listViewDevices;
+    private BluetoothAdapter bluetoothAdapter;
+    private BluetoothConnection bluetoothConnection;
+
+    private boolean isConnected = false;
+
+    private ArrayList<String> deviceList = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        /// BLUETOOTH
+        // get the bluetooth adapter
+        this.bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        bluetoothConnection = new BluetoothConnection(this.bluetoothAdapter);
+
+        // turn on bluetooth
+        this.turnOnBluetooth();
+
         /// LIST VIEW
-        // Devices
-        listViewDevices =(ListView)findViewById(R.id.listViewDevices);
+        // get the list
+        this.listViewDevices =(ListView)findViewById(R.id.listViewDevices);
 
-        // get list of devices
-        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-        Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+        /// Switch
+        // get the switch
+        switchButton = (Switch) findViewById(R.id.discover_paired);
+        // set the switch off
+        switchButton.setChecked(false);
 
-        for(BluetoothDevice bt : pairedDevices)
-            deviceList.add(bt.getName());
+        // Switch listenner
+        switchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String statusSwitch1, statusSwitch2;
+                if (switchButton.isChecked())
+                    Toast.makeText(getApplicationContext(), "Switch button checked ", Toast.LENGTH_LONG).show();
+                else
+                    Toast.makeText(getApplicationContext(), "Switch button unchecked", Toast.LENGTH_LONG).show();
+            }
+        });
 
-        listViewDevices.setAdapter(new ArrayAdapter(this, android.R.layout.simple_list_item_1, deviceList));
+        // get list of bluetooth Devices
+        deviceList = bluetoothConnection.getListPairedBluetooth(deviceList);
 
+        // update the list
+        this.listViewUpdate();
 
-        // ON CLICK
-        listViewDevices.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        // On click list
+        this.listViewDevices.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Toast.makeText(getApplicationContext(), "ListView item : " + position + " & id : " + deviceList.get((int)id), Toast.LENGTH_LONG).show();
                 Intent controlsPage=new Intent(HomeActivity.this, ControlsActivity.class);
-                controlsPage.putExtra("device_name",deviceList.get((int)id));
+                controlsPage.putExtra("device_name", deviceList.get((int)id));
                 startActivity(controlsPage);
             }
         });
 
-        imgAboutButton =(ImageButton)findViewById(R.id.imageAboutButton);
-        imgAboutButton.setOnClickListener(new View.OnClickListener() {
+        /// Image Button
+        // On click logo --> about
+        this.imgAboutButton =(ImageButton)findViewById(R.id.imageAboutButton);
+        this.imgAboutButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(getApplicationContext(), "About button clicked", Toast.LENGTH_LONG).show();
@@ -61,5 +94,27 @@ public class HomeActivity extends AppCompatActivity {
                 startActivity(aboutPage);
             }
         });
+    }
+
+    /**
+     * Turn on bluetooth
+     * @param
+     */
+    private void turnOnBluetooth() {
+        if (!this.bluetoothAdapter.isEnabled()) {
+            Intent turnOn = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(turnOn, 0);
+            Toast.makeText(getApplicationContext(), "Turned on",Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(getApplicationContext(), "Already on", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    /**
+     * Update the listView
+     * @param
+     */
+    private void listViewUpdate(){
+        this.listViewDevices.setAdapter(new ArrayAdapter(this, android.R.layout.simple_list_item_1, this.deviceList));
     }
 }
